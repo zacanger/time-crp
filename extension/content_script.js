9dee25e347d6c793d00676abceff6d7248f97d4b
zacanger <zac@zacanger.com>
2017-03-15 13:40:03 -0600

    It already runs at start, so.

--- extension/content_script.js
+++ extension/content_script.js
@@ -1,8 +1,5 @@
-const measureCRP = () => {
-  const t = window.performance.timing
-  const interactive = t.domInteractive - t.domLoading
-  const dcl = t.domContentLoadedEventStart - t.domLoading
-  const complete = t.domComplete - t.domLoading
-  console.log({ interactive, dcl, complete })
-}
-document.body.onload = measureCRP()
+const t = window.performance.timing
+const interactive = t.domInteractive - t.domLoading
+const dcl = t.domContentLoadedEventStart - t.domLoading
+const complete = t.domComplete - t.domLoading
+console.log({ interactive, dcl, complete })
